e36bd9873d84eb022ee0e5449c2b7e3c047610f0
jinchangmingddd <69374938+jinchangmingddd@users.noreply.github.com>
2022-10-19 10:26:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -48,6 +48,37 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2016-05-10' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '魏玮宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'onqZ76GMiyQUEzP_LqQjVMhBpLik',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'mYR4kRNh5oVNIiivPeKi6DPAMSUJ4lI_qEQUh_3BsuQ',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '04-04',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '老婆', year: '2000', date: '04-04',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '*生日', name: '靳常明', year: '1999', date: '09-28',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2016', date: '05-10',
+        },
+        {
+          type: '节日', name: '爱上魏玮纪念日', year: '2016', date: '05-10',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2016-05-10' },
+      ],
+    }
   ],
 
 
